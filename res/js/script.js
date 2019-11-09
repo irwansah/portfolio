f6367cad10c2081175c34ecca88b9eff628abc82
irwansah <irwansah607@gmail.com>
2019-11-09 12:59:58 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -3,6 +3,26 @@
 	(c) 2013 @ElmahdiMahmoud
 */   
 $(document).ready(function(){
+    $.getJSON('data.json',function(data){                
+        var output="" 
+        var outslide=""
+        $.each(data, function(key,val){
+          output+= '<div class="content" style="background-image:url(\'content/images/'+val.img.img1+'\')">';
+          output+= '<div class="content-wrap"></div>';
+            output+= '<div class="content-place">';
+                output+= '<div class="content-menu">';
+                output+= '<a href="#" class="link eye" data-sl1="'+val.img.img1+'" data-sl2="'+val.img.img2+'" data-sl3="'+val.img.img3+'" data-sl4="'+val.img.img4+'" data-sl5="'+val.img.img5+'" data-title="'+val.title+'" data-desc="'+val.desc+'" data-tags="'+val.tags+'" data-site="'+val.site+'"><img src="res/img/icon_eye.svg" class="icon" alt="eye icon" title="Look"></a>';
+                output+= '<a href="'+val.site+'" class="link" target="_new"><img src="res/img/icon_chrome.svg" class="icon" alt="chrome icon" title="Browse"></a>';
+                output+= '</div>';
+                output+= '<div class="content-desc">'+val.title+'</div>';
+                output+= '</div>';
+          output+= '</div>';                
+        });
+        output+='<div class="clear"></div> <nav class="nav-content"><a href="#" id="loadMore">Load More</a></nav><div class="nav-content-err"><p>Ups! No More.</p></div>'
+        $('.project').html(output);
+        
+    });
+    
     $('.pop').hide();
     $(".content").slice(0, 6).show();
         $("#loadMore").on('click', function (e) {
